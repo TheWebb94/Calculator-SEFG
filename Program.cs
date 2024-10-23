962483ce904dbab6b17e2e25be4551e6a352d13e
TheWebb94 <alexwebb94@hotmail.co.uk>
2024-10-23 18:16:19 +0100

    Code Upload
    Initial upload of local files to github

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using Calculator_v8324;
+using System;
+
+namespace Calculator_app
+{
+   // double answer = operationResult;
+    internal class Program
+    {
+        static double num1, num2;
+        static string operation;
+        private static double previousAnswer;
+
+                // add comments in for readability
+                // look at enums for constant values (operands)
+
+                //check recursion when caling self is fine?
+
+
+        static void Main()
+        {
+            bool continueCalculating = true;
+            while (continueCalculating)
+            {
+                GetExpressionFromUser();
+
+                NumberIsDivisibleByZero(operation, num2);
+
+                double operationResult = PerformOperation();
+
+                Console.WriteLine($"{num1}{operation}{num2}={operationResult}");
+
+                previousAnswer = operationResult;
+
+                continueCalculating = UserInputs.AskToContinue();
+               
+            }
+        }
+   
+        private static void GetExpressionFromUser()
+        {
+            num1 = UserInputs.GetNumber(previousAnswer, 1);
+            operation = UserInputs.GetOperator();
+            num2 = UserInputs.GetNumber(previousAnswer, 2);
+        }
+
+        private static double PerformOperation()
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "^":
+                    result = Math.Pow(num1, num2);
+                    break;
+            }
+
+            return result;
+        }
+        public static bool NumberIsDivisibleByZero(string operator1, double number)
+        {
+            if (operator1 != "/")
+            {
+                return true;
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("Cannot divide number by zero, enter a valid divisor");
+                num2 = UserInputs.GetNumber(previousAnswer, 2);
+                return NumberIsDivisibleByZero(operation, num2);
+            }
+            return true;
+        }
+    }
+}
---
+++ UserInputs.cs
@@ -0,0 +1,75 @@
+﻿using Calculator_app;
+using System;
+
+namespace Calculator_v8324
+{
+    internal class UserInputs
+    {
+        public static double GetNumber(double previousAnswer, int updateNum)
+        {
+            double number;
+            switch (updateNum)
+            {
+                case 1:
+                    Console.WriteLine("Enter first number or enter 'ans' to use previous calculation result");
+                    break;
+                case 2:
+                    Console.WriteLine("Enter second number or enter 'ans' to use previous calculation result");
+                    break;
+                default:
+                    break;
+            }
+            string input = Console.ReadLine();
+            if(input == "ans")
+            {
+                number = previousAnswer;
+                return number;
+            }
+            if (double.TryParse(input, out number))
+                {
+                    return number;
+                }
+            else
+                {
+                    Console.WriteLine("Invalid input, please enter a valid number");
+                    return GetNumber(previousAnswer, 1);
+                }
+        }
+
+        public static string GetOperator()
+        {
+            while (true)
+            {
+                Console.WriteLine("Pick one of the following operators: (+ / - * / ^)");
+                string input = Console.ReadLine();
+            if (input == "+" || input == "-" || input == "*" || input == "/" || input == "^")
+            {
+                return input;
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator chosen. Please enter one of the following: +, -, *, / ^");
+            }
+            }
+        }
+        public static bool AskToContinue()
+        {
+
+            Console.WriteLine("Would you like to continue calculating? Please enter 'y' or 'n'");
+
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "n")
+            {
+                return false;
+            }
+            else if (input == "y")
+            {
+                return true;
+            }
+            Console.WriteLine("Invalid choice, please enter 'y' or 'n'.");
+            return AskToContinue();
+        }
+
+    }
+}
